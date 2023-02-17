92e916c5b152a2c284b5fdeeceeb237da3c873f5
Evgenii Goncharov <125558139+Hlammaster@users.noreply.github.com>
2023-02-17 14:29:33 +0300

    Update Main.java

--- src/main/java/ru/hlammaster/Main.java
+++ src/main/java/ru/hlammaster/Main.java
@@ -2,6 +2,8 @@ package ru.hlammaster;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Sirius!");
+        System.out.println("Hello Sirius!!!!!!!!");
+        
+        pacman alive
     }
 }
